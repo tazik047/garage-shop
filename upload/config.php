323cec3ef1bfd7dd9cfed15997833ebb1252c406
Stanislav Zadorozhnii <stas249501@gmail.com>
2017-02-17 00:51:25 +0200

    Update configs

---
+++ upload/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://garage-shop.zadorozhnii.cu.cc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://garage-shop.zadorozhnii.cu.cc/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/inetpub/garageShop/catalog/');
+define('DIR_SYSTEM', 'C:/inetpub/garageShop/system/');
+define('DIR_IMAGE', 'C:/inetpub/garageShop/image/');
+define('DIR_LANGUAGE', 'C:/inetpub/garageShop/catalog/language/');
+define('DIR_TEMPLATE', 'C:/inetpub/garageShop/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/inetpub/garageShop/system/config/');
+define('DIR_CACHE', 'C:/inetpub/garageShop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/inetpub/garageShop/system/storage/download/');
+define('DIR_LOGS', 'C:/inetpub/garageShop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/inetpub/garageShop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/inetpub/garageShop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'zadorozhnii.cu.cc');
+define('DB_USERNAME', 'garage');
+define('DB_PASSWORD', 'garageadmin');
+define('DB_DATABASE', 'garage_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
