b6eea87ec54c40f1b0b2afee68e53fa02b1c21c0
Stanislav Zadorozhnii <stas249501@gmail.com>
2017-02-17 23:50:50 +0200

    Update config[2]

--- admin-config.php
+++ admin-config.php
@@ -8,18 +8,18 @@ define('HTTPS_SERVER', 'http://garage-shop.zadorozhnii.cu.cc/admin/');
 define('HTTPS_CATALOG', 'http://garage-shop.zadorozhnii.cu.cc/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/inetpub/garageShop/admin/');
-define('DIR_SYSTEM', 'C:/inetpub/garageShop/system/');
-define('DIR_IMAGE', 'C:/inetpub/garageShop/image/');
-define('DIR_LANGUAGE', 'C:/inetpub/garageShop/admin/language/');
-define('DIR_TEMPLATE', 'C:/inetpub/garageShop/admin/view/template/');
-define('DIR_CONFIG', 'C:/inetpub/garageShop/system/config/');
-define('DIR_CACHE', 'C:/inetpub/garageShop/system/storage/cache/');
-define('DIR_DOWNLOAD', 'C:/inetpub/garageShop/system/storage/download/');
-define('DIR_LOGS', 'C:/inetpub/garageShop/system/storage/logs/');
-define('DIR_MODIFICATION', 'C:/inetpub/garageShop/system/storage/modification/');
-define('DIR_UPLOAD', 'C:/inetpub/garageShop/system/storage/upload/');
-define('DIR_CATALOG', 'C:/inetpub/garageShop/catalog/');
+define('DIR_APPLICATION', 'C:/garageShop/admin/');
+define('DIR_SYSTEM', 'C:/garageShop/system/');
+define('DIR_IMAGE', 'C:/garageShop/image/');
+define('DIR_LANGUAGE', 'C:/garageShop/admin/language/');
+define('DIR_TEMPLATE', 'C:/garageShop/admin/view/template/');
+define('DIR_CONFIG', 'C:/garageShop/system/config/');
+define('DIR_CACHE', 'C:/garageShop/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/garageShop/system/storage/download/');
+define('DIR_LOGS', 'C:/garageShop/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/garageShop/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/garageShop/system/storage/upload/');
+define('DIR_CATALOG', 'C:/garageShop/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
