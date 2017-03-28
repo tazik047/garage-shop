ebad43ae2d74355a8a3388af38cf6ef1a925ffea
Stanislav Zadorozhnii <stas249501@gmail.com>
2017-03-28 22:36:53 +0300

    Fix novaposhta_ie

--- upload/admin/model/sale/novaposhta_ei.php
+++ upload/admin/model/sale/novaposhta_ei.php
@@ -2,19 +2,15 @@
 
 class ModelSaleNovaPoshtaEI extends Model {
 	function getOrderProducts($order_id) {
-		$this->db->query( 'SELECT op.quantity, op.product_id, p.weight, p.weight_class_id, p.length, p.width, p.height FROM ' . DB_PREFIX . 'order_product AS op INNER JOIN ' . DB_PREFIX . 'product AS p ON op.product_id=p.product_id AND op.order_id=' . $order_id );
-		$query = ;
+        $query = $this->db->query( 'SELECT op.quantity, op.product_id, p.weight, p.weight_class_id, p.length, p.width, p.height FROM ' . DB_PREFIX . 'order_product AS op INNER JOIN ' . DB_PREFIX . 'product AS p ON op.product_id=p.product_id AND op.order_id=' . $order_id );
 		return $query->rows;
 	}
 
 	function getCitiesAreasByString($filter) {
-		$this->descriptionSelect(  );
-		$select = ;
+		$select = $this->descriptionSelect();
 		$this->db->query( 'SELECT c.' . $select . ', z.name FROM ' . DB_PREFIX . 'novaposhta_cities AS c INNER JOIN ' . DB_PREFIX . 'zone AS z ON c.Area=z.code AND c.' . $select . ' LIKE \'' . $filter . '%\'' )->rows;
 		$results = $data = array(  );
-		foreach ($results as ) {
-			[0];
-			$result = ;
+		foreach ($results as $result) {
 			$data[] = array( 'Description' => $result[$select], 'CityArea' => $result[$select] . ', ' . $result['name'] . ' обл.' );
 		}
 
@@ -22,10 +18,8 @@ class ModelSaleNovaPoshtaEI extends Model {
 	}
 
 	function getAddressByString($address_name, $city_ref) {
-		$this->descriptionSelect(  );
-		$select = ;
-		$this->db->query( 'SELECT Ref FROM ' . DB_PREFIX . 'novaposhta_warehouses WHERE ' . $select . '=\'' . $address_name . '\' AND CityRef=\'' . $city_ref . '\'' )->row;
-		$result = ;
+		$select = $this->descriptionSelect();
+        $result = $this->db->query( 'SELECT Ref FROM ' . DB_PREFIX . 'novaposhta_warehouses WHERE ' . $select . '=\'' . $address_name . '\' AND CityRef=\'' . $city_ref . '\'' )->row;
 		return (isset( $result['Ref'] ) ? $result['Ref'] : false);
 	}
 
